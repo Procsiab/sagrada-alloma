a5e88c3586f1be58f9961bcde0b461cafde31be1
mattiasu96 <mattiasu96@live.it>
2018-03-28 22:12:49 +0200

    Creazione animazione di log in del gioco. Caricato titolo gioco con animazione, background e css relativo.
    L'immagine è stata inserita tramite percorso file locale, quindi potrebbe crear problemi nella compilazione su altri PC. In ogni caso ho caricato anche il file relativo, basterà aggiornare il Path nella classe Interface alla riga 28.
    Ho rimosso la classe e il package MainCode. Discutere di quale file verrà effettivamente utilizzato come main.

--- src/main/java/Interface/Interface.java
+++ src/main/java/Interface/Interface.java
@@ -1,4 +1,65 @@
 package Interface;
+import javafx.animation.ScaleTransition;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
-public class Interface {
+import java.io.FileInputStream;
+
+public class Interface extends Application {
+
+    private final String TITLE = "Center in Stage";
+    private final double WIDTH = 1280;
+    private final double HEIGHT = 720;
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setTitle(TITLE);
+
+        Image image = new Image(new FileInputStream("C:\\Users\\Mattia\\IdeaProjects\\ids-2018\\src\\main\\java\\Interface\\Sagrada.png"));
+        ImageView imageView = new ImageView(image);
+        imageView.setFitHeight(350);
+        imageView.setFitWidth(550);
+        imageView.setPreserveRatio(true);
+
+        BorderPane pane = new BorderPane();
+        pane.setCenter(imageView);
+        pane.setId("pane");
+        //Creating scale Transition
+        ScaleTransition scaleTransition = new ScaleTransition();
+
+        //Setting the duration for the transition
+        scaleTransition.setDuration(Duration.millis(1000));
+
+        //Setting the node for the transition
+        scaleTransition.setNode(imageView);
+
+        //Setting the dimensions for scaling
+        scaleTransition.setByY(1.5);
+        scaleTransition.setByX(1.5);
+
+        //Setting the cycle count for the translation
+        scaleTransition.setCycleCount(50);
+
+        //Setting auto reverse value to true
+        scaleTransition.setAutoReverse(false);
+
+        //Playing the animation
+        scaleTransition.play();
+
+        Scene scene = new Scene(pane, WIDTH, HEIGHT);
+        scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
