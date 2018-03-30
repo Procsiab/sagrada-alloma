e0d858f91adcfe13aca28af435745fdcba10b5b3
mattiasu96 <mattiasu96@live.it>
2018-03-30 21:28:56 +0200

    Modifiche all'animazione. Creata transizione al secondo layout tramita pulsante di start game. Da migliorare organizzazione del codice. Inserire Background schermata iniziale e poi procedere alla creazione del GridPane per inserire le carte nel secondo layout.

--- src/main/java/Interface/Interface.java
+++ src/main/java/Interface/Interface.java
@@ -1,36 +1,53 @@
 package Interface;
 import javafx.animation.ScaleTransition;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.ImagePattern;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import javafx.scene.control.Button;
 
+import java.awt.*;
 import java.io.FileInputStream;
 
 public class Interface extends Application {
-
+    private Scene scene, scene2;
     private final String TITLE = "Center in Stage";
     private final double WIDTH = 1280;
     private final double HEIGHT = 720;
-
+    private final String IMAGE_PATH =
+            "http://icons.iconarchive.com/icons/iconka/meow-2/64/cat-rascal-icon.png";
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
         primaryStage.setTitle(TITLE);
+        // CARICAMENTO IMMAGINE BACKGROUND LOGIN
+        Image image = new Image(new FileInputStream("C:\\Users\\Mattia\\IdeaProjects\\ids-2018\\src\\main\\java\\Interface\\Sagrada.png"));
 
-        Image image = new Image(new FileInputStream("Sagrada.png"));
         ImageView imageView = new ImageView(image);
         imageView.setFitHeight(350);
         imageView.setFitWidth(550);
         imageView.setPreserveRatio(true);
+        // CARICAMENTO TERMINATO
+        //CREAZIONE GRIGLIA BACKGROUND
+        Image image2 = new Image(new FileInputStream("C:\\Users\\Mattia\\IdeaProjects\\InterfacciaDemo\\src\\SA01privateobjectivecardsFRONTv5_ITA_copia-page-001.jpg"));
+        ImageView background = new ImageView(image2);
+        // background.setRotate(90);
 
-        BorderPane pane = new BorderPane();
-        pane.setCenter(imageView);
-        pane.setId("pane");
+        background.setFitWidth(WIDTH);
+        background.setFitHeight(HEIGHT);
+
+        Button StartGame = new Button("Start Game");
+        StartGame.setOnAction(e -> primaryStage.setScene(scene2));
+
+
         //Creating scale Transition
         ScaleTransition scaleTransition = new ScaleTransition();
 
@@ -45,18 +62,26 @@ public class Interface extends Application {
         scaleTransition.setByX(1.5);
 
         //Setting the cycle count for the translation
-        scaleTransition.setCycleCount(50);
+        scaleTransition.setCycleCount(10);
 
         //Setting auto reverse value to true
         scaleTransition.setAutoReverse(false);
 
         //Playing the animation
         scaleTransition.play();
-
-        Scene scene = new Scene(pane, WIDTH, HEIGHT);
-        scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(imageView,StartGame);
+        layout1.setAlignment(Pos.CENTER);
+        VBox layout2 = new VBox(20);
+        layout2.setAlignment(Pos.CENTER);
+        layout2.getChildren().addAll(background);
+        scene = new Scene(layout1,WIDTH,HEIGHT);
+        scene2 = new Scene(layout2,WIDTH,HEIGHT);
+        // scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.show();
+
+
     }
 
     public static void main(String[] args) {
