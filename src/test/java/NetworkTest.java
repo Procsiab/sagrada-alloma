bbd5d876cfd71adac58917dd4e27ea420a00646a
Procsiab <lerokamut@gmail.com>
2018-04-23 17:33:06 +0200

    First test implementations

--- src/test/java/NetworkTest.java
+++ src/test/java/NetworkTest.java
@@ -8,6 +8,8 @@ import java.rmi.RemoteException;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import shared.Logger;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.*;
@@ -32,8 +34,13 @@ public class NetworkTest {
     public void exportTest() {
         myNetServer.remotize(bar);
         myNetServer.export(bar, "bar");
-        Foo myFoo = (Foo) myNetClient.getExportedObject("bar");
-        Assert.assertEquals(bar.getName(), myFoo.getName());
+        SharedFoo myFoo = (SharedFoo) myNetClient.getExportedObject("bar");
+        try {
+            Assert.assertEquals(bar.getName(), myFoo.getName());
+        } catch (RemoteException re) {
+            Logger.log("Error calling remote method getName() through reference myFoo");
+            Logger.strace(re);
+        }
     }
 }
 
