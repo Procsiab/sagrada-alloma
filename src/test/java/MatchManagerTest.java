767f58f1aff42dadd63e79e5164da93251c17180
Procsiab <lerokamut@gmail.com>
2018-04-24 12:44:30 +0200

    First test implementations

--- src/test/java/MatchManagerTest.java
+++ src/test/java/MatchManagerTest.java
@@ -1,4 +1,3 @@
-/*
 import client.gui.StartGameController;
 import org.junit.Test;
 import server.MatchManager;
@@ -12,6 +11,7 @@ import shared.SharedClientGame;
 import shared.SharedServerMatchManager;
 
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
@@ -32,6 +32,14 @@ public class MatchManagerTest {
         assertThat(MatchManager.getInstance(), is(myMatchManager));
     }
 
-
+    @Test
+    public void startGameTest() {
+        final String CONNECTED = "Connection successful. Please wait for other players to connect";
+        final String REFUSED = "Too many incoming requests, please try again later. Sorry for that.";
+        MatchManager.setInstance();
+        for (int i = 0; i < MatchManager.MAX_ACTIVE_PLAYER_REFS; i++) {
+            assertEquals(CONNECTED, MatchManager.getInstance().startGame(myGameController));
+        }
+        assertEquals(REFUSED, MatchManager.getInstance().startGame(myGameController));
+    }
 }
-*/
