2cda2c1a2d1d3b908ccef5dbdd451b0544079f39
Bertox94 <37112695+Bertox94@users.noreply.github.com>
2018-05-01 09:15:33 +0200

    Start transition towards RMI + Socket

--- src/main/java/server/SReferences.java
+++ src/main/java/server/SReferences.java
@@ -9,6 +9,7 @@ public class SReferences {
     //MatchManager is singleton.
     public static Integer activePlayers;
     //given the id of a player
+    public static ArrayList<String> uUIDRef = new ArrayList<>();
     public static ArrayList<Integer> gameIdRef = new ArrayList<>();
     public static ArrayList<Player> playerRef = new ArrayList<>();
     public static ArrayList <GameManager> gameRef = new ArrayList<>();
