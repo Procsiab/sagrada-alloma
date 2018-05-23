b32c2336a81feee18538e328e2076d05f1bb7677
Bertox94 <37112695+Bertox94@users.noreply.github.com>
2018-05-23 17:47:52 +0200

    Public Objective hard implemented

--- src/main/java/shared/cards/publicOC/PublicOC10.java
+++ src/main/java/shared/cards/publicOC/PublicOC10.java
@@ -8,57 +8,43 @@ import shared.abstracts.PublicOC;
 
 public class PublicOC10 extends PublicOC {
 
-    /*
     public Integer use(Player player) {
         Dice[][] dices = player.overlay.getDicePositions().clone();
-        return computate(dices, 'b', 0, 0) +
-                computate(dices, 'r', 0, 0) +
-                computate(dices, 'y', 0, 0) +
-                computate(dices, 'v', 0, 0) +
-                computate(dices, 'g', 0, 0);
-    }
-*/
-
-    public Integer use(Player player) {
-        Dice[][] dices = player.overlay.getDicePositions().clone();
-        return 0;
-        //return computate(dices, 'b', 0, 0, false);
-    }
-
-    /*
-        public Integer computate(Dice[][] dices, Character color, Integer r, Integer c) {
-
-            if (r > 3 || r < 0 || c > 4 || c < 0)
-                return 0;
-            if (dices[r][c] == null)
-                return 0;
-            if (dices[r][c].getColor() != color)
-                return 0;
-            dices[r][c] = null;
-            return 1 + computate(dices, color, r - 1, c - 1) +
-                    computate(dices, color, r - 1, c + 1) +
-                    computate(dices, color, r + 1, c + 1) +
-                    computate(dices, color, r + 1, c - 1);
+        int i = 0;
+        int j = 0;
+        int sum = 0;
+        while (i < 3) {
+            j = 0;
+            while (j < 4) {
+                sum = sum + computate(dices, 'a', i, j, false);
+                j++;
+            }
+            i++;
         }
-    */
+        return sum;
+    }
 
-    /*
-    //add dA (diagonally adj in modo che non conti per ognuno la prima volta)
+
     public Integer computate(Dice[][] dices, Character color, Integer r, Integer c, Boolean dA) {
-
-        if(dA)
 
         if (r > 3 || r < 0 || c > 4 || c < 0)
             return 0;
         if (dices[r][c] == null)
             return 0;
         if (dices[r][c].getColor() != color)
-            return computate(dices, dices[r][c].getColor(), r, c, false?);
+            return computate(dices, dices[r][c].getColor(), r, c, false);
+        if (dA) {
+            dices[r][c] = null;
+            return 1 + computate(dices, color, r - 1, c - 1, true) +
+                    computate(dices, color, r - 1, c + 1, true) +
+                    computate(dices, color, r + 1, c + 1, true) +
+                    computate(dices, color, r + 1, c - 1, true);
+        }
         dices[r][c] = null;
-        return 1 + computate(dices, color, r - 1, c - 1,false?) +
-                computate(dices, color, r - 1, c + 1,false?) +
-                computate(dices, color, r + 1, c + 1,false?) +
-                computate(dices, color, r + 1, c - 1,false?);
+        return computate(dices, color, r - 1, c - 1, true) +
+                computate(dices, color, r - 1, c + 1, true) +
+                computate(dices, color, r + 1, c + 1, true) +
+                computate(dices, color, r + 1, c - 1, true);
     }
-    */
+
 }
