0a516c18e27d454f77c28ec7dc4a7a88ffcb57b9
Mattia <mattiasu96@live.it>
2018-06-17 23:52:50 +0200

    Added last view and scoreboard. Works properly. Tomorrow i'm gonna fill the text with the real data. By the way the data is getting retrieved correctly

--- src/main/java/client/gui/WinnerController.java
+++ src/main/java/client/gui/WinnerController.java
@@ -1,4 +1,49 @@
 package client.gui;
 
-public class WinnerController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+// I TRIED SO HARD, AND GOT SO FAR, IN THE END I CAN'T DYNAMYCALLY ALLOCATE CUSTOM FXML ITEMS.
+
+public class WinnerController implements Initializable {
+    private ArrayList<String> nicks;
+    private ArrayList<Integer> scores;
+    private ArrayList<Boolean> winner;
+    @FXML
+    public VBox leaderBoard;
+
+
+    public WinnerController(ArrayList<String> nicks, ArrayList<Integer> scores, ArrayList<Boolean> winner) {
+        System.out.println("Costructor WINNER");
+        this.nicks=nicks;
+        this.scores=scores;
+        this.winner=winner;
+
+    }
+
+    @FXML
+    public void initialize(URL location, ResourceBundle resources)  {
+        System.out.println("Inizializzazione WINNER");
+        System.out.println("Contenuto nicks WINNER" + nicks);
+
+
+
+
+
+
+    }
+
+    public void setVariables(ArrayList<String> nicks, ArrayList<Integer> scores, ArrayList<Boolean> winner){
+        System.out.println("Settin variables");
+
+    }
+
+
+
 }
